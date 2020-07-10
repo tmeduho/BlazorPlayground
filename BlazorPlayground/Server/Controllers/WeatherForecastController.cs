aae426d7572cec303ef07cb931ca21cbb71aceb0
Tyler Meduho <37983668+tmeduho@users.noreply.github.com>
2020-07-10 18:37:49 -0400

    run code cleanup and quick formatting

--- BlazorPlayground/Server/Controllers/WeatherForecastController.cs
+++ BlazorPlayground/Server/Controllers/WeatherForecastController.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
@@ -12,16 +11,16 @@ namespace BlazorPlayground.Server.Controllers
   [Route("[controller]")]
   public class WeatherForecastController : ControllerBase
   {
-    private static readonly string[] Summaries = new[]
+    private static readonly string[] Summaries =
     {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
+      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
 
-    private readonly ILogger<WeatherForecastController> logger;
+    private readonly ILogger<WeatherForecastController> _logger;
 
     public WeatherForecastController(ILogger<WeatherForecastController> logger)
     {
-      this.logger = logger;
+      _logger = logger;
     }
 
     [HttpGet]
